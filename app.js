b48323a795385faec98ea27bed22e369defa7728
mapfap <mapfaps@gmail.com>
2016-08-13 23:04:23 +0700

    Simplify as REST API

--- app.js
+++ app.js
@@ -1,46 +1,36 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users  = require('./routes/users');
-
 var app = express();
+var bodyParser = require('body-parser');
+var models  = require('./models');
+var api = express.Router();
+var v1 = express.Router(); 
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.get('/', function (req, res) {
+  res.send('Esso App API');
 });
 
-// error handler
-// no stacktraces leaked to user unless in development environment
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: (app.get('env') === 'development') ? err : {}
+v1.route('/users')
+
+  .post(function(req, res) {
+    models.User.create({
+      username: Math.random()//req.body.username
+    }).then(function() {
+      res.json({ message: '201'});
+    });
+  })
+
+  .get(function(req, res) {
+    models.User.findAll({
+      // include: [ models.Task ]
+    }).then(function(users) {
+      res.send(users);
+    });
   });
-});
 
+api.use('/v1', v1)
+app.use('/api', api);
 
 module.exports = app;
